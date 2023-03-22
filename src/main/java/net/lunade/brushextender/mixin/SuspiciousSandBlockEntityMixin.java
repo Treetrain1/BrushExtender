cd78310a709b0b4887368c138d9765cdc526f2ba
Treetrain1 <treetrain1@gmail.com>
2023-03-22 13:42:52 -0500

    23w12a

--- src/main/java/net/lunade/brushextender/mixin/SuspiciousSandBlockEntityMixin.java
+++ src/main/java/net/lunade/brushextender/mixin/SuspiciousSandBlockEntityMixin.java
@@ -5,13 +5,13 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.SuspiciousSandBlockEntity;
+import net.minecraft.world.level.block.entity.BrushableBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(SuspiciousSandBlockEntity.class)
+@Mixin(BrushableBlockEntity.class)
 public class SuspiciousSandBlockEntityMixin implements Brushable {
 
 	@Shadow
